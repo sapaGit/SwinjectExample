3bbc77b51381c07237aa27b3186a81988b5cf995
Sergey Pavlov <mosevaly@gmail.com>
2023-05-26 20:00:04 +0300

    Initial Commit

---
+++ SwinjectExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwinjectExample
+//
+//  Created by Sergey Pavlov on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
